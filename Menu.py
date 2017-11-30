6191b7127f17cb93f95859019c61c7019a1d4f6b
1-21-01-22B-22\pc1-21-01-22B-03 <edgar.alquinga@epn.edu.ec>
2017-11-30 10:06:49 -0500

    ahora si acabo :v ya vale todo jejejej

--- Menu.py
+++ Menu.py
@@ -1,43 +1,49 @@
+import os
 pi = 3.14
 res = 0
-print ("Menu")
-print ("Calculo de areas")
-print ("1.- Circulo")
-print ("2.- Cuadrado")
-print ("3.- Rectangulo")
-print ("4.- Triangulo")
-print("5.- Salir")
+salir = False
 
-op = int(input("Seleccione un opcion"))
+def funcion():
+    print ("Menu")
+    print ("Calculo de areas")
+    print ("1.- Circulo")
+    print ("2.- Cuadrado")
+    print ("3.- Rectangulo")
+    print ("4.- Triangulo")
+    print ("5.- Salir")
 
-while(op != 5):
+while True:
+
+    funcion()
+    op = int(input("Seleccione un opcion"))
+
     if op == 1:
-
-        radio = int(input("Ingrese el radio del circulo"))
-        res = pi * (radio*radio)
+        radio = float(input("Ingrese el radio del circulo"))
+        res = pi * (radio * radio)
         print("El area del circulo es: " + str(res))
-        op = 5
 
-    if op == 2:
 
-        la1 = int(input("Ingrese la altura del cuadrado"))
-        la2 = int(input("Ingrese la del base"))
-        res = la1*la2
+    elif op == 2:
+        la1 = float(input("Ingrese la altura del cuadrado"))
+        la2 = float(input("Ingrese la del base"))
+        res = la1 * la2
         print("El area del cuadrado es: " + str(res))
-        op = 5
 
-    if op == 3:
 
-        la1r = int(input("Ingrese la altura del cuadrado"))
-        la2r = int(input("Ingrese la base"))
-        res = la1r*la2r
+    elif op == 3:
+        la1r = float(input("Ingrese la altura del cuadrado"))
+        la2r = float(input("Ingrese la base"))
+        res = la1r * la2r
         print("El area del resctangulo es: " + str(res))
-        op = 5
 
 
-    if op == 4:
-        alt = int(input("Ingrese la altura del triangulo"))
-        baset = int(input("Ingrese la base del triangulo"))
-        res = (baset*alt)/2
+    elif op == 4:
+        alt = float(input("Ingrese la altura del triangulo"))
+        baset = float(input("Ingrese la base del triangulo"))
+        res = (baset * alt) / 2
         print("El area del resctangulo es: " + str(res))
-        op = 5
+
+    elif op == 5:
+        break
+    else:
+        print ("Opcion no valida :v seleccione una opcion correcta")
