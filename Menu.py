9618f9c0cab3c45d328b05780e64c55fa7b20025
1-21-01-22B-22\pc1-21-01-22B-03 <edgar.alquinga@epn.edu.ec>
2017-11-30 09:39:47 -0500

    ahora si el programa ya termina :v

--- Menu.py
+++ Menu.py
@@ -12,23 +12,32 @@ op = int(input("Seleccione un opcion"))
 
 while(op != 5):
     if op == 1:
+
         radio = int(input("Ingrese el radio del circulo"))
         res = pi * (radio*radio)
         print("El area del circulo es: " + str(res))
+        op = 5
+
     if op == 2:
 
         la1 = int(input("Ingrese la altura del cuadrado"))
         la2 = int(input("Ingrese la del base"))
         res = la1*la2
         print("El area del cuadrado es: " + str(res))
+        op = 5
+
     if op == 3:
+
         la1r = int(input("Ingrese la altura del cuadrado"))
         la2r = int(input("Ingrese la base"))
         res = la1r*la2r
         print("El area del resctangulo es: " + str(res))
+        op = 5
+
 
     if op == 4:
         alt = int(input("Ingrese la altura del triangulo"))
         baset = int(input("Ingrese la base del triangulo"))
         res = (baset*alt)/2
         print("El area del resctangulo es: " + str(res))
+        op = 5
