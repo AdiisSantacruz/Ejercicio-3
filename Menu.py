98d569d5a056d21d59bc0ad342b082dbad3a8319
1-21-01-22B-22\pc1-21-01-22B-03 <edgar.alquinga@epn.edu.ec>
2017-11-30 09:36:09 -0500

    acabe :v

--- Menu.py
+++ Menu.py
@@ -3,7 +3,7 @@ res = 0
 print ("Menu")
 print ("Calculo de areas")
 print ("1.- Circulo")
-print ("2.- Cuadrad")
+print ("2.- Cuadrado")
 print ("3.- Rectangulo")
 print ("4.- Triangulo")
 print("5.- Salir")
@@ -15,12 +15,20 @@ while(op != 5):
         radio = int(input("Ingrese el radio del circulo"))
         res = pi * (radio*radio)
         print("El area del circulo es: " + str(res))
+    if op == 2:
 
+        la1 = int(input("Ingrese la altura del cuadrado"))
+        la2 = int(input("Ingrese la del base"))
+        res = la1*la2
+        print("El area del cuadrado es: " + str(res))
+    if op == 3:
+        la1r = int(input("Ingrese la altura del cuadrado"))
+        la2r = int(input("Ingrese la base"))
+        res = la1r*la2r
+        print("El area del resctangulo es: " + str(res))
 
-la1 = int(input("Ingrese la altura del cuadrado"))
-la2 = int(input("Ingrese la altura del base"))
-la1r = int(input("Ingrese la altura del cuadrado"))
-la2r = int(input("Ingrese la base"))
-alt = int(input("Ingrese la altura del triangulo"))
-baset = int(input("Ingrese la base del triangulo"))
-
+    if op == 4:
+        alt = int(input("Ingrese la altura del triangulo"))
+        baset = int(input("Ingrese la base del triangulo"))
+        res = (baset*alt)/2
+        print("El area del resctangulo es: " + str(res))
